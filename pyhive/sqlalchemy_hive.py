b3a528f84576b284ef6b5cb5cb4da3ad25a7f9da
Baeg-il Kim <dt101665@mobis-partners.com>
2024-11-29 09:12:04 +0900

    revert colspecs

--- pyhive/sqlalchemy_hive.py
+++ pyhive/sqlalchemy_hive.py
@@ -396,7 +396,6 @@ class HiveDialect(default.DefaultDialect):
     supports_multivalues_insert = True
     supports_sane_rowcount = False
     supports_statement_cache = False
-    colspecs = {sqltypes.Interval: HiveInterval}
 
     @classmethod
     def dbapi(cls):
