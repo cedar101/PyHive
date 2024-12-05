d8aa8e719b580762db3e9688125a449c747a19e0
Baeg-il Kim <dt101665@mobis-partners.com>
2024-12-05 17:32:20 +0900

    append to super()

--- pyhive/sqlalchemy_hive.py
+++ pyhive/sqlalchemy_hive.py
@@ -202,45 +202,10 @@ class HiveDDLCompiler(DDLCompiler):
     """
 
     def visit_primary_key_constraint(self, constraint, **kw):
-        def _text():
-            if constraint.name is not None:
-                formatted_name = self.preparer.format_constraint(constraint)
-                if formatted_name is not None:
-                    yield f"CONSTRAINT {formatted_name}"
-
-            yield "PRIMARY KEY ({}) DISABLE NOVALIDATE RELY".format(
-                ", ".join(
-                    self.preparer.quote(c.name)
-                    for c in (
-                        constraint.columns_autoinc_first
-                        if constraint._implicit_generated
-                        else constraint.columns
-                    )
-                )
-            )
-
-        return "".join(_text()) if constraint else ""
+        return f"{super().visit_primary_key_constraint(constraint)} DISABLE NOVALIDATE RELY"
 
     def visit_foreign_key_constraint(self, constraint, **kw):
-        preparer = self.preparer
-
-        def _text():
-            if constraint.name is not None:
-                formatted_name = self.preparer.format_constraint(constraint)
-                if formatted_name is not None:
-                    yield f"CONSTRAINT {formatted_name} "
-            elements_iter = iter(constraint.elements)
-            remote_table = next(elements_iter).column.table
-            yield "FOREIGN KEY({}) REFERENCES {} ({}) DISABLE NOVALIDATE".format(
-                ", ".join(preparer.quote(f.parent.name) for f in constraint.elements),
-                self.define_constraint_remote_table(constraint, remote_table, preparer),
-                ", ".join(preparer.quote(f.column.name) for f in constraint.elements),
-            )
-            yield self.define_constraint_match(constraint)
-            yield self.define_constraint_cascades(constraint)
-            yield self.define_constraint_deferrability(constraint)
-
-        return "".join(_text())
+        return f"{super().visit_foreign_key_constraint(constraint)} DISABLE NOVALIDATE"
 
     def post_create_table(self, table):
         """Build table-level CREATE options."""
